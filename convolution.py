9dd69fb3924a842fe5928daf413159517359837e
Imammahdi170625 <imammahdizakaria75@gmail.com>
2022-03-20 15:23:57 +0600

    error removed

--- convolution.py
+++ convolution.py
@@ -3,12 +3,12 @@ m.reverse()
 g = [[1, 1, 1], [1, 0, 1]]
 
 ans = [[] for i in range(len(g))]
-
+print(ans)
 for idx, x in enumerate(g):
     padded_m = ([0] * (len(x) - 1)) + m + ([0] * (len(x) - 1))
     i = len(padded_m)
     while i > len(x) - 1:
-        result = [num1 * num2 for num1, num2 in zip(padded_m[i - 3:i], x)]
+        result = [num1 * num2 for num1, num2 in zip(padded_m[i - len(x):i], x)]
         ans[idx].append(sum(result) % 2)
         i = i - 1
 print(f"ans: {ans}")

4ed6d0bc4cc6d2711c6b2a1d686eae13a6f20ace
Imammahdi170625 <imammahdizakaria75@gmail.com>
2022-03-21 00:24:48 +0600

    hamming and entropy added

---
+++ hammingCode.py
@@ -0,0 +1,40 @@
+import math
+
+msg = [1, 0, 1, 0, 1]
+msg.reverse()
+m = len(msg)
+r = 1
+
+while pow(2, r) < m + r + 1:
+    r += 1
+
+code = [0 for i in range(m + r + 1)]
+for i in range(r):
+    code[pow(2, i)] = -1
+j = 0
+for i in range(1, len(code)):
+    if code[i] != -1:
+        code[i] = msg[j]
+        j += 1
+
+for i in range(1, len(code)):
+    if code[i] == -1:
+        count = 0
+        # print(f" for {i}: ")
+        for j in range(i + 1, len(code)):
+            val = 1 << int(math.log2(i))
+            if j & val == val:
+                # print(j, end=" ")
+                if code[j] == 1:
+                    count += 1
+        print()
+        if count % 2 == 0:
+            code[i] = 0
+        else:
+            code[i] = 1
+msg.reverse()
+# code.reverse()
+idx = [i for i in range(1, len(code))]
+print(f"Message bits are: {msg}")
+print(f"Hamming code idx: {idx}")
+print(f"Hamming code is:  {code[1:]}")
